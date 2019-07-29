d9daf04029f0f7eead65666faefe857f57c371f8
DANIEL <daniel@daniel.local.lan>
2019-07-29 14:36:56 -0300

    Ajuste do Form

--- apps/registro_hora_extra/urls.py
+++ apps/registro_hora_extra/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
-from .views import HoraExtraList
+from .views import HoraExtraList, HoraExtraEdit, HoraExtraDelete, HoraExtraNovo
 
 urlpatterns = [
     path('', HoraExtraList.as_view(), name='list_hora_extra'),
-    # path('editar/<int:pk>/', EmpresaEdit.as_view(), name='edit_empresa'),
+    path('novo/', HoraExtraNovo.as_view(), name='create_registro_hora_extra'),
+    path('editar/<int:pk>/', HoraExtraEdit.as_view(), name='edit_hora_extra'),
+    path('deletar/<int:pk>/', HoraExtraDelete.as_view(), name='delete_hora_extra'),
+
 ]
